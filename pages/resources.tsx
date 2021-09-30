da71b7be426a5e7d0b4d2c14895f4cada4ab9b8c
Justin Chiou <chiou.kai@gmail.com>
2021-09-30 14:30:26 -0400

    feat: venue information in /resources

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,5 +1,62 @@
+import Image from "next/image";
+import Link from "next/link";
+import CardImg from "react-bootstrap/CardImg";
+
 const ResourcesPage = () => {
-  return <h1>Resources</h1>;
+  return (
+    <div className="container py-5">
+      <h1 className="text-center pb-5">Resources</h1>
+      <p className="text-center">
+        Currently, we use the following venues for our events:
+      </p>
+      <section className="row">
+        <div className="col-12 col-md-6">
+          <div className="card">
+            <CardImg
+              as={Image}
+              src="/goc.jpeg"
+              alt="Greek Orthodox Church image"
+              width={200}
+              height={200}
+              layout="responsive"
+            />
+            <div className="card-body">
+              <h5 className="card-title">
+                Holy Trinity - St. Nicholas Greek Orthodox Church
+              </h5>
+              <address>7000 Winton Rd, Cincinnati, OH 45224</address>
+              <Link href="https://goo.gl/maps/YTGbnvZZTb1jUb4RA" passHref>
+                <a target="_blank" rel="noreferrer" className="btn btn-primary">
+                  Directions
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <div className="card">
+            <CardImg
+              as={Image}
+              src="/music_hall.jpg"
+              alt="Music Hall image"
+              width={200}
+              height={200}
+              layout="responsive"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Cincinnati Music Hall</h5>
+              <address>1241 Elm St, Cincinnati, OH 45202</address>
+              <Link href="https://goo.gl/maps/F8SbajhF6ayt58sf9" passHref>
+                <a target="_blank" rel="noreferrer" className="btn btn-primary">
+                  Directions
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default ResourcesPage;
