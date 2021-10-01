2a94435b8154c11b16c13b4d2ca6f991b48312a2
Justin Chiou <chiou.kai@gmail.com>
2021-09-30 23:05:15 -0400

    feat: embedded calendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,5 +1,18 @@
 const CalendarPage = () => {
-  return <h1>Calendar</h1>;
+  return (
+    <div className="container">
+      <h1 className="text-center py-5">Calendar of Events</h1>
+      <div className="row d-flex justify-content-center">
+        <iframe
+          src="https://calendar.google.com/calendar/embed?src=ioq3a5he808t30kg2o95o03a30%40group.calendar.google.com&ctz=America%2FNew_York&mode=AGENDA"
+          width="800"
+          height="600"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default CalendarPage;
