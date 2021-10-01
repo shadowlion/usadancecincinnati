64bc6a8d24839fd2406b391f3dd180f1c87d21ef
Justin Chiou <chiou.kai@gmail.com>
2021-09-30 23:05:09 -0400

    feat: current board members

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,27 @@ import type { NextPage } from "next";
 
 const HomePage: NextPage = () => {
   return (
-    <div className="container py-5">
-      <h1 className="text-center">
+    <div className="container">
+      <h1 className="text-center py-5">
         Your connection to Ballroom Dancing in the Tri-State area.
       </h1>
+      <div className="row d-flex justify-content-center">
+        <div className="col-12 col-md-4">
+          <h2>Current Board Members:</h2>
+          <ul>
+            <li>President - Carol Ward</li>
+            <li>Vice President - Louise Buker</li>
+            <li>Treasurer - Yasha Ogg</li>
+            <li>Secretary - Justin Chiou</li>
+            <li>Member at Large - Katie Jones</li>
+            <li>Member at Large - Joy Ingram</li>
+            <li>Member at Large - Brenda Karr</li>
+            <li>Member at Large - Michelle Weber</li>
+            <li>Member at Large - Jeff Greatorex</li>
+            <li>Member at Large - Greg Howard</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
